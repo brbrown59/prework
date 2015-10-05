894af68297d168070e6add256759d5d1814cf151
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-05 15:02:06 -0600

    More experiments performed

--- experiments.php
+++ experiments.php
@@ -8,15 +8,45 @@
 </body>
 <?php
 $firstname = "Pete";
+$middlename = "Alexander";
 $lastname = "Jones";
 
-$fullname = combine_names ($firstname, $lastname);
+//call function to combine names into one array
+$fullname = combine_names ($firstname, $middlename, $lastname);
 
-echo $fullname;
-
-function combine_names ($n1, $n2)
+foreach($fullname as $print_name)
 {
-	$combined = $n1 . " " . $n2;
+	echo "$print_name ";
+}
+
+sort($fullname, SORT_STRING);
+
+//puts the names in alphabetical order, demonstrate foreach use
+foreach($fullname as $print_alpha_name)
+{
+	echo "<br>$print_alpha_name";
+}
+
+$street = "Easy Street";
+$house = "123";
+$city = "Oakland";
+$state = "New Mexico";
+
+//instead of calling function, use built in one!!!
+$address = compact('house', 'street', 'city', 'state');
+
+foreach($address as $print_address)
+{
+	echo "<br>$print_address";
+}
+
+
+//combines names into one array
+function combine_names ($n1, $n2, $n3)
+{
+	$combined = array('first' => $n1,
+							'middle' => $n2,
+							'last' => $n3);
 	return $combined;
 }
 
